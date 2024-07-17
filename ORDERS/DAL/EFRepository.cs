ac6837ff0b57423b42849e82f14cfcf6c6500c8b
Andres Pinzon <andrespinzonxd@gmail.com>
2024-07-17 13:57:35 -0500

    feat(back-end): Add class EFRepository with method CreateAsync() fix

--- ORDERS/DAL/EFRepository.cs
+++ ORDERS/DAL/EFRepository.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using DAL.Datos;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    internal class EFRepository
+    public class EFRepository : IRepository
     {
+        //Llamada a la clase de contexto
+        ApplicationDbContext _context;
+
+        //Constructor
+        public EFRepository(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        //Dispose
+        private bool _disposedValue;
+
+        public async Task<TEntity> CreateAsync<TEntity>(TEntity toCreate) where TEntity : class
+        {
+            TEntity Result = default(TEntity);
+            try
+            {
+                await _context.Set<TEntity>().AddAsync(toCreate);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbException) 
+            { 
+                throw;
+            }
+            return Result;
+        }
+
+        public Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> Criteria) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> RetrieveAsync<TEntity>(Expression<Func<TEntity, bool>> Criteria) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
     }
 }
